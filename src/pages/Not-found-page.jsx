d87b114b2ef68b6a1a2ca7107dd6288a0fdbb1a0
Imasha_Dilshan <imashadilshan1080@gmail.com>
2025-12-01 13:25:33 +0530

    define Not-Found Page

---
+++ src/pages/Not-found-page.jsx
@@ -0,0 +1,9 @@
+function Notfoundpage() {
+  return (
+    <div className="text-center mt-20"> 
+      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+export default Notfoundpage;
