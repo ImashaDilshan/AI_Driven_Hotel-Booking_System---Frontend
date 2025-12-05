20af898ad6287496b479f2b4ac141f8a4ee85d21
Imasha_Dilshan <imashadilshan1080@gmail.com>
2025-12-05 20:12:45 +0530

    upadate to the then catch patten to the async await patten

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,22 +1,40 @@
-const getAllHotels = () => {
+// async await pattern (modern)
 
-    // Make an request to the API endpoint to fetch all hotels
+const getAllHotels = async () => {
 
-    const res = fetch ("http://localhost:8000/api/HotelData" , {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json" // mention the expected data format
-        },
-    }).then((res) => {
-        return res.json(); // Parse the response as JSON But that also return a promise 
-    }).then((data) => {
-         // Return the parsed data
-        console.log(data);
-    })
-    .catch((error) => {
-        console.error("Error fetching hotel data:", error);
-    });
-    
+        const res = await fetch ("http://localhost:8000/api/HotelData" , {
+            method: "GET",
+            headers: { "content-Type": "application/json" }
+        });
+        if (!res.ok) {
+            throw new Error ("Failed to fetch hotel data");
+        }
+        const data = await res.json();
+         //console.log(data);
+        return data; // Return the parsed data
 }
 
 export { getAllHotels };
+
+//     // then catch chain patten (old)
+
+// const getAllHotels = () => {
+//     // Make an request to the API endpoint to fetch all hotels
+
+//     const res = fetch ("http://localhost:8000/api/HotelData" , {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json" // mention the expected data format
+//         },
+//     }).then((res) => {
+//         return res.json(); // Parse the response as JSON But that also return a promise 
+//     }).then((data) => {
+//          // Return the parsed data
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.error("Error fetching hotel data:", error);
+//     });
+    
+// }
+// export { getAllHotels };
