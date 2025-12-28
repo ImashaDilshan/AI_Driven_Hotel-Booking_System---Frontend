aca7ae55287f475464f44df03aec92bf374107d8
Imasha_Dilshan <imashadilshan1080@gmail.com>
2025-12-28 23:57:47 +0530

    update location Tab

--- src/LocationsTab.jsx
+++ src/LocationsTab.jsx
@@ -2,17 +2,22 @@
 function LocationsTab(props) {
 
     const handleClick = () => {
-        //console.log("when the tab click:",props.Location);
+        //console.log("when the tab click:",key);
         props.onClick(props.Location);
+        const key = props.Location.name;
+     console.log("when the tab click:",key);
+        
     }
 
-
+    
     return (
         <div className="text-base border-b-blue-100 border rounded-full px-2 py-1 cursor-pointer " 
         onClick={handleClick}
         >
             {props.Location.name}
+            
         </div>
+         
     );
 }
 export default LocationsTab;
