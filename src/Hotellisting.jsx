26b302f9b206077e6bf5f291e98c8d933846ebcc
Imasha_Dilshan <imashadilshan1080@gmail.com>
2026-01-05 00:47:13 +0530

    Async State management defining

--- src/Hotellisting.jsx
+++ src/Hotellisting.jsx
@@ -25,7 +25,7 @@ function Hotellisting() {
         getAllHotels()
             .then((data) => {
                 setHotelData(data);
-                //console.log("Fetched Hotel Data:", data);
+                console.log("Fetched Hotel Data:", data);
             })
             .catch((error) => {
                 //console.error("Error fetching hotel data:", error);
