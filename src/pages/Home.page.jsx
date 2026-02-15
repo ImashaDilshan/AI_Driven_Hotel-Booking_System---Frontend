3b8f0e146a4efd4a0ebd5c0500e751854c02da45
Imasha_Dilshan <imashadilshan1080@gmail.com>
2026-02-15 23:40:21 +0530

    Nothing change anything so far

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -5,20 +5,19 @@ import React from 'react';
 import Hotellisting from '../Hotellisting';
 import { Button } from '../components/ui/button';
 import { getAllHotels } from '../lib/Api';
-import { useSelector } from 'react-redux';
+
 
 
 
 function Homepage() {
 
-  const counter = useSelector((state) => state.counter.value);
-  console.log("Counter Value in Home Page:", counter);
+
 
   return (
     <>
    
     <Hotellisting />
-     <p>Counter Value: {counter}</p>
+    
     {/* <Button onClick={()=> getAllHotels()}  > Get Hotel Data</Button> */}
    </>
   )
