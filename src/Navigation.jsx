cc19ccb46b065b29b8570c3b0ad14a09e2a00208
Imasha_Dilshan <imashadilshan1080@gmail.com>
2026-03-12 12:18:53 +0530

    Clerk login indicator config

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,12 +1,10 @@
 import { Button } from "./components/ui/button";
 import { Globe } from "lucide-react";
 import  { Link } from "react-router";
-import { useSelector } from 'react-redux';
+import { SignedIn,SignedOut,UserButton,useUser } from "@clerk/clerk-react";
 
 function Navigation() {
 
-    const counter = useSelector((state) => state.counter.value);
-  console.log("Counter Value in Navigation:", counter);
 
   return (
     <nav className="z-50 bg-black/90 backdrop-blur-md flex items-center justify-between px-4 sm:px-6 text-white py-3 rounded-full mx-4 my-3 relative">
@@ -14,7 +12,7 @@ function Navigation() {
             <Link to="/" className="text-xl font-bold">Booking 360</Link>
          <div className="hidden md:flex space-x-8">
             <Link to='/' className="transition-colors text-sm">Home</Link>
-             <p> {counter}</p>
+           
         </div>
         </div>
 
@@ -23,13 +21,22 @@ function Navigation() {
                 <Globe className="h-6 w-6 mr-2" />
                 En
             </Button>
-            <Button variant="outline" size="sm" className="bg-black text-xs text-yellow-300 hover:bg-yellow-500 hover:text-black hidden md:flex">
+
+            <SignedOut>
+              <Button variant="outline" size="sm" className="bg-black text-xs text-yellow-300 hover:bg-yellow-500 hover:text-black hidden md:flex">
                 <Link to="/login">Login</Link>
-            </Button>
-            <Button variant="ghost" size="sm" 
-            className="bg-white text-black text-xs hover:bg-yellow-100 text-xs hidden md:flex">
+              </Button>
+              <Button variant="ghost" size="sm" className="bg-white text-black text-xs hover:bg-yellow-100 text-xs hidden md:flex">
                 <Link to="/signup">Sign Up</Link>
-            </Button>
+              </Button>
+            </SignedOut>
+
+            <SignedIn>
+                <UserButton/>
+                <Button size="sm" asChild className="bg-white text-black hover:bg-gray-200 text-xs hidden md:flex">
+                        <Link to="/account">My Account</Link>
+                </Button>
+            </SignedIn>
 
 
         </div>
